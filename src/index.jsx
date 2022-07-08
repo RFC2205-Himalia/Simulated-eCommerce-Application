de4fafcbfa3a1136826d5a64797beb174d7d5a30
rhosmans <rhosmans@gmail.com>
2022-07-08 10:26:35 -0600

    more setup complete, added esLinst and babel

---
+++ src/index.jsx
@@ -0,0 +1,29 @@
+// Bring React in to build a component. 
+import React from "react";
+// Bring reactDOM in to mount component to the dom.
+import reactDOM from "react-dom";
+
+// Here is out base App component. 
+// Notice we are NOT using jsx here. This is because we have not set up babel yet.
+const App = () => {
+    return <h1>Hello Nick</h1>
+  }
+// Render our app to the dom mounted to the element with id of root inside our public/index.html file.
+reactDOM.render(<App />, document.getElementById("app"));
+
+
+
+/*
+
+import { createRoot } from "react-dom/client";
+const root = createRoot(document.getElementById("root"));
+
+// Huzzah for jsx!
+const App = () => {
+  return <h1>Hello World</h1>
+}
+
+root.render(<App />);
+
+
+*/
