5f82a3fd2518ba2e01754c439d50ad862fcf7dad
David Fan <davidlfan@gmail.com>
2022-07-13 01:45:31 -0400

    Got basic tests working, but having trouble with the async parts of the testing

--- client/src/components/Overview/Product Details/ProductDetails.test.js
+++ client/src/components/Overview/Product Details/ProductDetails.test.js
@@ -1,13 +1,29 @@
-// import React from 'react';
-// import {act, render, fireEvent} from "@testing-library/react";
-// import ProductDetails from "./ProductDetails.jsx";
+import React from 'react';
+import {render as rtlRender, act, fireEvent, waitFor, screen} from "@testing-library/react";
+import ProductDetails from "./ProductDetails.jsx";
+import { Provider } from 'react-redux';
+import store from '../../../reducer.js'
+
+const render = component => rtlRender(
+<Provider store = {store}>
+  {component}
+</Provider>
+)
 
 
-// describe("Product Details Component", () => {
-//   it("rendered components", () => {
-//     const { getByTestId } = render(<ProductDetails />)
-//     const productName = getByTestId("productName")
-//     expect(productName).toBe("Morning Joggers")
-//   })
 
-// })
+describe("Product Details Component", () => {
+  it("rendered product name", () => {
+    const { getByTestId } = render(<ProductDetails />)
+    const productName = getByTestId("productName")
+    expect(productName).toBeInTheDocument;
+  })
+
+  it("rendered correct name", async () => {
+      const { getByTestId } = render(<ProductDetails />)
+      const productName = getByTestId("productName")
+      await waitFor(() => expect(productName.innerHTML).toBe('Morning Joggers'), {timeout: 4000})
+
+  })
+
+})
