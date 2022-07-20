1acc09f81ce2f25d4699486e6849ff815006fdd7
rhosmans <rhosmans@gmail.com>
2022-07-20 03:03:05 -0600

    characteristics bars now showing what each poriton of the bar means

--- client/src/components/Review/Characteristics.jsx
+++ client/src/components/Review/Characteristics.jsx
@@ -2,12 +2,57 @@ import React from "react";
 import styled from "styled-components";
 
 function Characteristics({ characteristics }) {
-
+    const CharScaleMeanings = {
+        Size: ["Too small", "Perfect", "Too large"],
+        Width:["Too Narrow", "Perfect", "Too Wide"],
+        Comfort: ["Poor", "Ok", "Perfect"],
+        Quality:["Poor", "What I Expected", "Perfect"],
+        Length:["Runs Short", "Perfect", "Runs Long"],
+        Fit:["Runs Tight", "Perfect", "Runs Long"]
+      };
 
 
   return (
     <CharacteristicsWrapper>
       {Object.keys(characteristics).map((characteristic, i) => {
+        let lowerEnd = 'low';
+        let middle = 'middle'
+        let highEnd = 'high';
+
+        // console.log(characteristic);
+
+        switch(characteristic){
+            case "Size":
+                lowerEnd = CharScaleMeanings.Size[0];
+                middle = CharScaleMeanings.Size[1];
+                highEnd = CharScaleMeanings.Size[2];
+                break;
+            case "Width":
+                lowerEnd = CharScaleMeanings.Width[0];
+                middle = CharScaleMeanings.Width[1];
+                highEnd = CharScaleMeanings.Width[2];
+                break;
+            case "Comfort":
+                lowerEnd = CharScaleMeanings.Comfort[0];
+                middle = CharScaleMeanings.Comfort[1];
+                highEnd = CharScaleMeanings.Comfort[2];
+                break;
+            case "Quality":
+                lowerEnd = CharScaleMeanings.Quality[0];
+                middle = CharScaleMeanings.Quality[1];
+                highEnd = CharScaleMeanings.Quality[2];
+                break;
+            case "Length":
+                lowerEnd = CharScaleMeanings.Length[0];
+                middle = CharScaleMeanings.Length[1];
+                highEnd = CharScaleMeanings.Length[2];
+                break;
+            case "Fit":
+                lowerEnd = CharScaleMeanings.Fit[0];
+                middle = CharScaleMeanings.Fit[1];
+                highEnd = CharScaleMeanings.Fit[2];
+                break;
+        }
 
         return (
           <form key={characteristics[characteristic].id}>
@@ -18,6 +63,12 @@ function Characteristics({ characteristics }) {
               <CharacteristicsScale/>
               <CharacteristicsScale/>
             </Indicator>
+            <ScaleText>
+                <LowEnd>{lowerEnd}</LowEnd>
+                <MidText>{middle}</MidText>
+                <HighEnd>{highEnd}</HighEnd>
+            </ScaleText>
+
           </form>
         );
       })}
@@ -60,6 +111,27 @@ const CharacteristicsWrapper = styled.div`
 const CharacteristicsText = styled.span`
     font-weight: 500;
     font-size: 17px;
+    display: flex;
+`;
+
+const ScaleText = styled.div`
+    font-weight: 500;
+    font-size: 17px;
+    display: flex;
+    justify-content: space-evenly;
+`;
+
+const HighEnd = styled.div`
+
+`;
+// margin-left: 70%;    
+
+const LowEnd = styled.div`
+
+`;
+
+const MidText = styled.div`
+
 `;
 
 
