9ad4c9a75b1197d25c209960f69990a8fca34e94
rhosmans <rhosmans@gmail.com>
2022-07-22 03:05:37 -0600

    added recommended percentage, review summary functionally complete

---
+++ client/src/components/Review/ReviewRecommendations.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { useSelector } from "react-redux";
+import styled from 'styled-components';
+import { useEffect, useState } from "react";
+
+
+
+function ReviewRecommendations ({data}) {
+    data = data || {"false": "1", "true": "22" }
+    let [percentageRec, setPercentageRec] = useState(0);
+
+    let calcPercentage = (metaData) => {
+        let falseNum = parseInt(metaData.false);
+        let trueNum = parseInt(metaData.true);
+        // console.log(typeof trueNum);
+
+        let percentRec = Math.floor((trueNum/(trueNum + falseNum))*100)
+        setPercentageRec(percentRec)
+    }
+
+    useEffect(() => {
+        calcPercentage(data);
+      }, [data]); 
+
+
+
+    return (
+        <Recommended>
+            <div>{percentageRec}% of people Recommended this Item!</div>
+        </Recommended>
+    )
+}
+
+const Recommended = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
+
+export default ReviewRecommendations;
